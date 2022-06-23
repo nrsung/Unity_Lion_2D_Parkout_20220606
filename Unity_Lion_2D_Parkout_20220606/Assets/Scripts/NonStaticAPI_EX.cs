ada4950f277a7e278627543cdf36e8724ce654c8
nrsung <48666091+nrsung@users.noreply.github.com>
2022-06-23 16:31:24 +0800

    Update 20220623習題

--- Unity_Lion_2D_Parkout_20220606/Assets/Scripts/NonStaticAPI_EX.cs
+++ Unity_Lion_2D_Parkout_20220606/Assets/Scripts/NonStaticAPI_EX.cs
@@ -4,12 +4,39 @@ namespace NRSUNG
 {
     public class NonStaticAPI_EX : MonoBehaviour
     {
-        SphereCollider myCollider;
+        // 1.定義欄位,資料類型為類別
+        // 2.Unity 屬性面板必須確定該欄位不是空值(None)
+        // 3.使用非靜態API
+        [SerializeField]private GameObject cube;
+        [SerializeField]private GameObject sphere;
+        [SerializeField]private GameObject capsule;
+
+        [SerializeField]private SphereCollider sphere_collider;
+        [SerializeField]private Camera cam;
+        //[SerializeField]
+        private Rigidbody rig;
+
+        private void Awake()
+        {
+            //ani = GetComponent<Animator>();
+            rig = GetComponent<Rigidbody>();
+        }
+
         private void Start()
         {
             print("攝影機深度 : " + Camera.main.depth);
-            myCollider = GetComponent<SphereCollider>();
-            print("球體碰撞器半徑 : " + myCollider.radius);
+            //myCollider = GetComponent<SphereCollider>();
+            //print("球體碰撞器半徑 : " + myCollider.radius);
+            print("球體碰撞器半徑 : " + sphere_collider.radius);
+            cam.backgroundColor = Color.green;
+            capsule.transform.localScale = new Vector3 (3, 2, 1);
+        }
+
+        private void Update()
+        {
+            cube.transform.LookAt(sphere.transform.position);
+            sphere.transform.Rotate(0, 3, 0);
+            
         }
     }
 }
